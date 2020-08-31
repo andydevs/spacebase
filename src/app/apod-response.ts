7d6495d728ffcdaa8338786a39daaee7fb703737
Anshul Kharbanda <akanshul97@gmail.com>
2020-08-31 17:20:11 -0400

    Added support for images

--- src/app/apod-response.ts
+++ src/app/apod-response.ts
@@ -1,9 +1,10 @@
 import { SafeResourceUrl } from '@angular/platform-browser';
 
 export interface ApodResponse {
+    title: string,
     date: string,
     explanation: string,
     media_type: string,
     service_version: string,
-    title: SafeResourceUrl
+    url: SafeResourceUrl
 }
