82f2f75bffc122da3de29ebce1980538d47b7787
Anshul Kharbanda <akanshul97@gmail.com>
2020-08-31 17:16:48 -0400

    APOD Url added as embedded video (And some url sanitization added)

--- src/app/apod-response.ts
+++ src/app/apod-response.ts
@@ -1,8 +1,9 @@
+import { SafeResourceUrl } from '@angular/platform-browser';
+
 export interface ApodResponse {
     date: string,
     explanation: string,
     media_type: string,
     service_version: string,
-    title: string,
-    url: string
+    title: SafeResourceUrl
 }
--- src/app/apod.service.ts
+++ src/app/apod.service.ts
@@ -3,6 +3,8 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { ApodResponse } from './apod-response';
 import { API_KEY } from './app.config';
+import { DomSanitizer } from '@angular/platform-browser';
+import { map } from 'rxjs/operators';
 
 @Injectable({
     providedIn: 'root'
@@ -10,13 +12,24 @@ import { API_KEY } from './app.config';
 export class ApodService {
     feedurl = 'https://api.nasa.gov/planetary/apod'
   
-    constructor(private http: HttpClient) {}
+    constructor(private http: HttpClient, private sanitizer: DomSanitizer) {}
   
     getData(): Observable<ApodResponse> {
+        // Get options
         let options = {
             params: new HttpParams()
                 .set('api_key', API_KEY)
         }
-        return this.http.get<ApodResponse>(this.feedurl, options)
+
+        // Update url
+        return this.http.get(this.feedurl, options)
+            .pipe<ApodResponse>(
+                map<any, ApodResponse>(
+                    body => ({
+                        ...body,
+                        url: this.sanitizer.bypassSecurityTrustResourceUrl(body.url)
+                    })
+                )
+            )
     }
 }
